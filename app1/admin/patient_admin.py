dced41f0ac415880c9018c2855835587f39b9d6c
Abdullah Hodan <abdullahhodan448@gmail.com>
2025-09-24 11:56:07 +0600

    hocche na

--- app1/admin/patient_admin.py
+++ app1/admin/patient_admin.py
@@ -5,7 +5,7 @@ from app1.models.patient_models import patient
 
 class Patientadmin(admin.ModelAdmin):
     list_display=('user','age','disease')
-    search_fields=('user__first_name','user__last_name','disease','email')
+    search_fields=('user__first_name','user__last_name','disease','user__email')
     list_filter=('age','disease','user__is_active','user__is_staff')
     ordering=('age',)
     list_editable=('age','disease')
